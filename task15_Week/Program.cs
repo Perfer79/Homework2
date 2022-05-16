294d5a4c7ef08f8a45209201b47aff2d70907b9b
Sergey <perfer79@gmail.com>
2022-05-16 15:48:43 +0700

    Решена задача 15

--- task15_Week/Program.cs
+++ task15_Week/Program.cs
@@ -3,4 +3,16 @@
 7 -> да
 1 -> нет
 */
-Console.WriteLine("Hello, World!");
+Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine("Выходной? -> О да!");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
+    Console.WriteLine("В неделе 7 дней");
+  }
+  else Console.WriteLine("Выходной? -> к сожалению нет");
+
