a0d4a820160b591d9e92d30fcc7ca9db5fcdb177
Sergey <perfer79@gmail.com>
2022-05-16 14:13:34 +0700

    Добавлены условия задач

---
+++ task10_2nd_Digit/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Введите трехзначное число");
+Console.ReadLine();
+
+int rundomNumber = (100,1000);
+Console.WriteLine(rundomNumber);
+int numberOne = (rundomNumber / 100) * 10;
+int numberTwo = rundomNumber % 10;
+Console.WriteLine($"результат{numberOne + numberTwo}");
